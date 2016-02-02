607064c25aaed4716621b3b42ae6998548e4c4c7
dej <david@redfroggy.fr>
2016-02-02 16:57:04 +0100

    add dist folder

---
+++ dist/angular-ui-router-state-history.dev.js
@@ -0,0 +1,92 @@
+/**
+ * AngularJS service to store last state params used for each state.
+ * @version v0.0.1 - 2016-02-02
+ * @link https://github.com/RedFroggy/angular-ui-router-state-history
+ * @author David jay <david.jay@redfroggy.fr>
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
+
+
+'use strict';
+
+
+// Source: src/angular-ui-router-state-history.js
+
+/**
+ * Created by David JAY on 29/01/2016.
+ * Store params of state and reload then when state is called again
+ */
+(function() {
+
+    /**
+     * The state history module
+     */
+    var stateHistory = angular.module('rf.state.history',
+        [
+            'ui.router',
+            'angular-storage'
+        ]);
+
+    /**
+     * override the $state's method transitionTo to store params of the state
+     * @param $provide
+     */
+    var stateHistoryConfigure = function ($provide) {
+        var $state_transitionTo;
+        $provide.decorator("$state", ['$delegate', 'stateStore', function ($state, stateStore) {
+            $state_transitionTo = $state.transitionTo;
+            $state.transitionTo = function (to, toParams, options) {
+                var toState = $state.get(to);
+                if (toParams == null && stateStore.get(toState.name) !== undefined){
+                    toParams =  stateStore.get(to);
+                    arguments[1] = toParams;
+                }
+
+                return $state_transitionTo.apply($state, arguments).then(
+                    function (result) {
+                        stateStore.set(toState.name, toParams);
+                        return result;
+                    },
+                    function (err) {
+                        stateStore.remove(toState.name);
+                        return $q.reject(err);
+                    }
+                );
+            };
+            return $state;
+        }]);
+    };
+
+    // angular module configuration
+    stateHistory.config(["$provide", stateHistoryConfigure]);
+
+    /**
+     * state's store service manage the history states
+     */
+    var stateStoreProvider = function () {
+
+        // the default storage
+        var _storage = 'localStorage';
+
+        /**
+         * Sets the storage.
+         *
+         * @param {String} storage The storage name
+         */
+        this.setStorage = function(storage) {
+            if (storage && angular.isString(storage)) {
+                _storage = storage;
+            }
+        };
+
+        this.$get = ['store',function(store) {
+            var stateStore = store.getNamespacedStore('state',_storage);
+
+            return stateStore;
+        }];
+    };
+
+    // add stateStore service to the module
+    stateHistory.provider('stateStore', [stateStoreProvider]);
+
+}());
